4732d2d0cd4417c8566ba95c478ebcef50b5dacb
Mikkel J <mikkelgj@gmail.com>
2012-02-20 13:31:54 +0100

    testing git

--- src/edu/aau/utzon/UtzonActivity.java
+++ src/edu/aau/utzon/UtzonActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class UtzonActivity extends Activity {
     /** Called when the activity is first created. */
+
+    // roflcopter git er awesome
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
