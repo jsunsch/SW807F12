4dfdf02a34fdf14fa3a4438730c07d878b87954f
steffan88 <spalle08@student.aau.dk>
2012-02-20 12:47:38 +0100

    Added the WebserviceActivity to the Android manifest file

--- gen/edu/aau/utzon/R.java
+++ gen/edu/aau/utzon/R.java
@@ -16,6 +16,7 @@ public final class R {
     }
     public static final class id {
         public static final int OutdoorActivity=0x7f050001;
+        public static final int WebserviceActivity=0x7f050002;
         public static final int mapview=0x7f050000;
     }
     public static final class layout {
