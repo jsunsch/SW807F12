3ad16475347c2d1a283c1ee67c05cce703f3d4e5
= <mikkelgj@gmail.com>
2012-02-20 20:41:55 +0100

    Implemented first "prototype" of the content provider. Does not utilize the restful method(s)

---
+++ src/edu/aau/utzon/webservice/ProviderContract.java
@@ -0,0 +1,54 @@
+package edu.aau.utzon.webservice;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public final class ProviderContract {
+	public  static final String AUTHORITY = "edu.aau.utzon";
+	
+	// This class cannot be instantiated
+    private ProviderContract() {
+    }
+    
+    /*
+     * "Points" table
+     */
+    public static final class Points implements BaseColumns {
+    	// This class cannot be instantiated
+    	private Points() {}
+    	
+    	public static final String TABLE_NAME = "points";
+    	
+        private static final String SCHEME = "content://";
+    	
+    	// Attributes
+    	public static final String ATTRIBUTE_ID = "_ID";
+    	public static final String ATTRIBUTE_X = "X";
+    	public static final String ATTRIBUTE_Y = "Y";
+    	public static final String ATTRIBUTE_DESC = "DESC";
+    	
+    	// URI Paths
+    	public static final String PATH_POINTS = "/points";
+    	public static final String PATH_POINT_ID = "/points/";
+    	
+    	// Position of ID
+    	public static final int POINT_ID_PATH_POSITION = 1;
+    	
+    	// The content:// style URL for this table
+    	public static final Uri CONTENT_URI =  Uri.parse(SCHEME + AUTHORITY + PATH_POINTS);
+    	
+    	/**
+         * The content URI base for a single note. Callers must
+         * append a numeric note id to this Uri to retrieve a note
+         */
+    	public static final Uri CONTENT_ID_URI_BASE = Uri.parse(SCHEME + AUTHORITY + PATH_POINT_ID);
+    	
+    	/**
+         * The content URI match pattern for a single note, specified by its ID. Use this to match
+         * incoming URIs or to construct an Intent.
+         */
+        public static final Uri CONTENT_ID_URI_PATTERN = Uri.parse(SCHEME + AUTHORITY + PATH_POINT_ID + "/#");
+        
+        public static final String DEFAULT_SORT_ORDER = ATTRIBUTE_ID + " ASC";
+    }
+}
