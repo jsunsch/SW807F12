155a064d093284fc757f35d98ea0fc9df826d1ec
steffan88 <steffan88@gmail.com>
2012-02-22 22:25:57 +0100

    Improved calibration system. NOT algorithmicly

--- src/edu/aau/utzon/indoor/CalibrationActivity.java
+++ src/edu/aau/utzon/indoor/CalibrationActivity.java
@@ -1,6 +1,8 @@
 package edu.aau.utzon.indoor;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.List;
 
 import edu.aau.utzon.R;
@@ -20,63 +22,103 @@ public class CalibrationActivity extends Activity  {
 	WifiManager _wifi;
 	EditText _editText;
 	TextView _textView;
-	
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.calibration);
-		
+
 		_editText = (EditText)findViewById(R.id.editText2);  
 		_textView = (TextView)findViewById(R.id.textView1);
-		
+
 		String connectivity_context = Context.WIFI_SERVICE;
 		_wifi = (WifiManager)getSystemService(connectivity_context);
-		
+
 		_textView.setText("Number of points: " + PointData.getPoints().size());
 	}
-	
-	public void addPoint(View view) {
+
+	public void addPoint(View view) throws InterruptedException {
 		if (_wifi.startScan() == true)
 		{
-			List<ScanResult> scanResults =  _wifi.getScanResults();
+			Hashtable<String, Integer> measures = new Hashtable<String, Integer>();
+			Hashtable<String, Integer> wifiCounts = new Hashtable<String, Integer> ();
+			
+			for (int i = 0; i < 4*5; i++) {
 
-			ArrayList<WifiMeasure> measures = new ArrayList<WifiMeasure>();
-			for (ScanResult res : scanResults) {
-				if (-res.level < 80)
-					measures.add(new WifiMeasure(res.BSSID, -res.level));
+				List<ScanResult> scanResults =  _wifi.getScanResults();
+
+				for (ScanResult res : scanResults) {
+					if (-res.level < 80) {
+
+						if (measures.containsKey(res.BSSID) == false) {
+							measures.put(res.BSSID, new Integer(-res.level));
+						}
+						else {
+							int newValue = (Integer)measures.get(res.BSSID);
+							newValue += -res.level;
+							measures.remove(res.BSSID);
+							measures.put(res.BSSID, new Integer(newValue));
+						}
+
+						if (wifiCounts.containsKey(res.BSSID) == false) {
+							wifiCounts.put(res.BSSID, new Integer(1));
+						}
+						else {
+							int newValue = (Integer)wifiCounts.get(res.BSSID);
+							newValue++;
+							wifiCounts.remove(res.BSSID);
+							wifiCounts.put(res.BSSID, new Integer(newValue));
+						}
+					}
+				}
+
+				Thread.sleep(250);
+				_wifi.startScan();
 			}
 			
-			PointData.addPoint(new Point(measures, _editText.getText().toString()));
-			
-			new AlertDialog.Builder(this)
-		    .setTitle("Done")
-		    .setMessage("Point added!")
-		    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-		        public void onClick(DialogInterface dialog, int which) { 
+			ArrayList<WifiMeasure> realMeasures = new ArrayList<WifiMeasure>(); 
 
-		        }
-		     })
-		     .show();
+		    Enumeration<String> keys = measures.keys();
+		    
+		    while(keys.hasMoreElements()) {
+		       String key = keys.nextElement();
+		       int value = (Integer)measures.get(key);
+		       
+		       WifiMeasure wm = new WifiMeasure(key, value / wifiCounts.get(key));
+		       realMeasures.add(wm);
+		    }
+		  
+			PointData.addPoint(new Point(realMeasures, _editText.getText().toString()));
+
+			new AlertDialog.Builder(this)
+			.setTitle("Done")
+			.setMessage("Point added!")
+			.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) { 
+
+				}
+			})
+			.show();
 		}
 		else
 		{
 			new AlertDialog.Builder(this)
-		    .setTitle("Error")
-		    .setMessage("Could not scan for WIFI networks!")
-		    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-		        public void onClick(DialogInterface dialog, int which) { 
+			.setTitle("Error")
+			.setMessage("Could not scan for WIFI networks!")
+			.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) { 
 
-		        }
-		     })
-		     .show();
+				}
+			})
+			.show();
 		}
 		//PointData.AddPoint(new Point())
-		
+
 		_textView.setText("Number of points: " + PointData.getPoints().size());
 	}
-	
+
 	public void deleteAllPoints(View view)  {
 		PointData.deleteAllPoints();
-		
+
 		_textView.setText("Number of points: " + PointData.getPoints().size());
 	}
 }
