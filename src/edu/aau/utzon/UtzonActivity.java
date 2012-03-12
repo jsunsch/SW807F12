a190f5f72f8abbcb49a5766ed57d9cce640de940
steffan88 <steffan88@gmail.com>
2012-03-12 15:20:34 +0100

    Made a change in the main activity so that the webservice activity whont be started. For some reason the webservice activity crashes when it is started. I will now try to merge the indoor branch with the master branch

--- src/edu/aau/utzon/UtzonActivity.java
+++ src/edu/aau/utzon/UtzonActivity.java
@@ -1,20 +1,17 @@
 package edu.aau.utzon;
 
-import edu.aau.utzon.indoor.IndoorActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
 public class UtzonActivity extends Activity {
     /** Called when the activity is first created. */
-
-    // roflcopter git er awesome
-
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         //startActivity(new Intent(getApplicationContext(), OutdoorActivity.class));
-        startActivity(new Intent(getApplicationContext(), IndoorActivity.class));
+        //startActivity(new Intent(getApplicationContext(), WebserviceActivity.class));
     }
 }
