e8c8c27885ab08abaa6ff910fb432212d99f8623
steffan88 <spalle08@student.aau.dk>
2012-03-21 13:47:27 +0100

    Mostly a test commit. But also contains a few start classes

---
+++ src/edu/aau/utzon/PoiListActivity.java
@@ -0,0 +1,5 @@
+package edu.aau.utzon;
+
+public class PoiListActivity {
+
+}
---
+++ src/edu/aau/utzon/location/PointOfInterest.java
@@ -0,0 +1,13 @@
+package edu.aau.utzon.location;
+
+public class PointOfInterest {
+	String mLocationName;
+
+	public String getmLocationName() {
+		return mLocationName;
+	}
+
+	public void setmLocationName(String mLocationName) {
+		this.mLocationName = mLocationName;
+	}
+}
