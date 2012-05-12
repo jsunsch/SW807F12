7d4440ff79e791441030e43043311e813930ffc1
= <mikkelgj@gmail.com>
2012-05-12 20:54:18 +0200

    Update

--- src/edu/aau/utzon/location/SampleService.java
+++ src/edu/aau/utzon/location/SampleService.java
@@ -1,12 +1,18 @@
 package edu.aau.utzon.location;
 
+import java.util.List;
 import java.util.Random;
 
 import android.app.IntentService;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -16,29 +22,80 @@ public class SampleService extends Service {
 	private final IBinder mBinder = new SampleBinder();
 	// Random number generator
 	private final Random mGenerator = new Random();
-	
+
 	/**
 	 * Class used for the client Binder.  Because we know this service always
 	 * runs in the same process as its clients, we don't need to deal with IPC.
 	 */
-    public class SampleBinder extends Binder {
-    	SampleService getService() {
-            return SampleService.this;
-        }
-    	
-    	LocationHelper getLocationHelper() {
-    		return mLocationHelper;
-    	}
-    }
-    
-    LocationHelper mLocationHelper = null;
-    private void initState(Context context) {
-    	mLocationHelper = new LocationHelper(context);
-    }
+	public class SampleBinder extends Binder {
+		SampleService getService() {
+			return SampleService.this;
+		}
+
+		LocationHelper getLocationHelper() {
+			return mLocationHelper;
+		}
+	}
+
+	LocationHelper mLocationHelper = null;
+	LocationListener mLocationListener = null;
+	LocationManager mLocationManager = null;
+	private void initState(Context context) {
+		mLocationHelper = new LocationHelper(context);
+		mLocationListener = new LocationListener() {
+			
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+				
+				
+			}
+			
+			@Override
+			public void onProviderEnabled(String provider) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProviderDisabled(String provider) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onLocationChanged(Location location) {
+				mLocationHelper.makeUseOfNewLocation(location);
+				
+			}
+		};
+	}
+
+	private void disableLocationListener() {
+		mLocationManager.removeUpdates(mLocationListener);
+	}
+	
+	private String getBestProvider() {
+		Criteria c = new Criteria();
+		c.setAccuracy(Criteria.ACCURACY_COARSE);
+		return mLocationManager.getBestProvider(c, true);
+	}
+	
+	private void enableLocationListener() {
+		mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		mLocationManager.requestLocationUpdates(getBestProvider(), 0, 0, mLocationListener);
+	}
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		initState(this);
+	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		initState(this);
+		
+		enableLocationListener();
+		
 		return mBinder;
 	}
 
@@ -46,10 +103,10 @@ public class SampleService extends Service {
 	public int getRandomNumber() {
 		return mGenerator.nextInt(100);
 	}
-	
+
 	public LocationHelper getLocationHelper() {
 		Log.d(TAG, "getLocationHelper()");
 		return mLocationHelper;
 	}
-	
+
 }
